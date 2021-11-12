170228f473c61f8f17396742a7b8a1899cf4bfd8
Grenky <84088519+Grenky@users.noreply.github.com>
2021-11-12 15:24:03 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+   $('.slider-inner').slick({
+      dots: true,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/arrow-left.svg" alt="arrow-left"></button>',
+      nextArrow:'<button type="button" class="slick-next"><img src="images/slider/arrow-right.svg" alt="arrow-right"></button>',
+   });
+
+
+});
